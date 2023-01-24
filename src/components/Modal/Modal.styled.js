59664d63a40e0fd036691b70c66c218c7243677f
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-01-24 23:07:09 +0200

    changes

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -14,6 +14,7 @@ export const Overlay = styled.div`
   justify-content: center;
   align-items: center;
   background-color: rgba(0, 0, 0, 0.8);
+  z-index: 1200;
 `;
 
 export const ModalWindow = styled.div`
